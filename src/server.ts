4b52f3531cb25589fd80cfad2c94b516b4bfa17d
Thiagohrcosta <thiagohrcosta86@gmail.com>
2023-02-21 01:51:23 -0300

    adding sql queries

--- src/server.ts
+++ src/server.ts
@@ -4,12 +4,15 @@ import { knex } from './database'
 const app = fastify()
 
 app.get('/hello', async () => {
-  const tables = await knex('sqlite_schema').select('*')
-  return tables
+  const transactions = await knex('transactions').select('*')
+
+  return transactions
 })
 
 app
   .listen({
     port: 3333,
   })
-  .then(() => console.log('HTTP Server running on port 3333'))
+  .then(() => {
+    console.log('HTTP Server Running!')
+  })
