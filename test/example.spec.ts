e130eb5a95cf54c1ad88df6fe35b96b2cf7f374b
Thiagohrcosta <thiagohrcosta86@gmail.com>
2023-02-22 11:15:32 -0300

    user can create a new transaction test

--- test/example.spec.ts
+++ test/example.spec.ts
@@ -1,3 +1,18 @@
-import { test } from 'vitest'
+import { test, beforeAll } from 'vitest'
+import request from 'supertest'
+import { app } from '../src/app'
 
-test('O usuário consegue criar uma nova transação', () => {})
+beforeAll(async () => {
+  await app.ready()
+})
+
+test('User can create a new transaction', async () => {
+  await request(app.server)
+    .post('/transactions')
+    .send({
+      title: 'New transaction',
+      amount: 5000,
+      type: 'credit',
+    })
+    .expect(201)
+})
